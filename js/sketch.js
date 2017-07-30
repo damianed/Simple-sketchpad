3551611d53bce2d2d1af8e85397bf8d07804f567
Damian Escobedo <damian050697@gmail.com>
2017-07-30 22:16:36 +0000

    added a change size button, and now when you hover it paints a random color instead of just black

--- js/sketch.js
+++ js/sketch.js
@@ -1,33 +1,59 @@
-var sqrSize = 16;
-var size = 90;
+var width = 500;
+var size = 15;
+var sqrSize;
 $(document).ready(function(){
-	drawBoard(size);
-	$('button#clearbtn').on('click', function(){
+	calcSqr(size);
+	drawBoard();
+	$('button#setSize').on('click', function(){
 		var tempS= prompt("Enter the size");
 		var tempS = parseInt(tempS, 10);
 
 		if(tempS!=null && Number.isInteger(tempS)){
-			$('#whiteboard').empty();
-			drawBoard(tempS);
+			calcSqr(tempS);
+			clear();
 		}
 	});
 
-});
+	$('button#clearbtn').on('click',function(){
+		clear();
+	})
 
-function drawBoard(s){
-	for(var i=0; i<s*16;i+=16){
+});
+function drawBoard(){
+	for(var i=0; i<width;i+=sqrSize){
 		$('#whiteboard').append("<div class='row' style='display: block;height:"+sqrSize+"px'><div>");
 		}
-	for(var i=0;i<s*16;i+=16){
+	for(var i=0;i<width;i+=sqrSize){
 		$('#whiteboard > div.row').append("<div class='sqr' style='height:"+sqrSize+"px;width:"+sqrSize+"px;'></div>");
 		}
 
 		setHover();
+		setButtonW();
 }
 
 function setHover(){
 	$('div.sqr').mouseenter(function(){
-		$(this).css('background-color', 'black');
+		$(this).css('background-color', getRandomColor());
 	});
 
+}
+
+function setButtonW(){
+	$('button#clearbtn').css({'width': ($('whiteboard').width()+'px')});
+}
+function calcSqr(s){
+	sqrSize=width/s;
+}
+
+function clear(){
+	$('#whiteboard').empty();
+	drawBoard();
+}
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
